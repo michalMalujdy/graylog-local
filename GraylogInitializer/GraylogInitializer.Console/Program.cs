f04ce52aa6630382ef9b8611be57265f318d8c5d
Michal Malujdy <mima@scalepoint.com>
2022-06-08 20:14:02 +0200

    Delete filebeat input, fix docker url

--- GraylogInitializer/GraylogInitializer.Console/Program.cs
+++ GraylogInitializer/GraylogInitializer.Console/Program.cs
@@ -1,9 +1,10 @@
 ﻿using GraylogInitializer.Console;
-using GraylogInitializer.Console;
 using Microsoft.Extensions.Configuration;
+using Serilog;
 
 var configuration = BuildConfiguration();
 var graylogApiService = new GraylogApiService(configuration);
+Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
 
 var retryCount = 0;
 const int maxRetries = 15;
@@ -12,12 +13,15 @@ while (retryCount < maxRetries)
 {
     try
     {
+        Log.Debug("Starting the Graylog initialization");
         await graylogApiService.EnsureInputs();
         await graylogApiService.EnsureStreams();
         break;
     }
-    catch (HttpRequestException)
+    catch (HttpRequestException e)
     {
+        Log.Error("Error: {error}", e.Message);
+
         retryCount++;
         await Task.Delay(5000);
     }
